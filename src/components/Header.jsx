795a919a4b6cef34a9f505b81f9c98d718157961
Yaroslav <faloout@gmail.com>
2022-09-15 17:21:17 +0300

    changes 14 lesson

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (<header className="header">
+                <img src="https://w7.pngwing.com/pngs/70/772/png-transparent-vimeo-computer-icons-logo-short-film-vitag-svg-miscellaneous-television-text-thumbnail.png" alt="" />
+           </header>);
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <img src="https://www.nps.gov/pore/planyourvisit/images/pic-north-beach-sunset-amk-210104-960x320_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false" alt="" />
+
+      <div className="">ava + desc</div>
+      <div className=""> My Posts
+          <div className="">New Post</div>
+          <div className=""> Post 1 
+
+          </div>
+          <div className="">Post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
