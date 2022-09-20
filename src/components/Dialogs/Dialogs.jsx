340dc0513a0a49b35c619c3d869b0cdf0105b7d8
Yaroslav <faloout@gmail.com>
2022-09-20 13:42:00 +0300

    Lesson 21 - Dialogs page (верстка)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,20 @@ import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-       <div>Dialogs</div>
+       <div className={s.dialogs}>
+         <div className={s.dialogsItems}>
+            <div className={s.dialog}>Name1</div>
+            <div className={s.dialog}>Name2</div>
+            <div className={s.dialog}>Name3</div>
+            <div className={s.dialog}>Name4</div>
+            <div className={s.dialog}>Name5</div>
+         </div>
+         <div className={s.messages}>
+            <div className={s.dialog}>Hi</div>
+            <div className={s.dialog}>How is your progress?</div>
+            <div className={s.dialog}>Yoo</div>
+         </div>
+       </div>
     );
   };
   
