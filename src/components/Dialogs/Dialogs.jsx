06e6010fd9f36ac3e3d5cf12e35260bd250a0b8a
Yaroslav <faloout@gmail.com>
2022-09-23 00:24:41 +0300

    26 : project tree! структура проекту; ще про ui - bll; Dialogs розбивка на комп

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
-
-const DialogsItem = (props) => {
-  let path = "/dialogs" + props.id;
-
-  return (
-    <div className={s.dialog + " " + s.active}>
-      <NavLink to={path}>{props.name}</NavLink>
-    </div>
-  );
-};
-
-const Message = (props) => {
-  return <div className={s.dialog}>{props.message}</div>;
-};
+import DialogsItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props) => {
   let dialogs = [
