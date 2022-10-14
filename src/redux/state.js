17aed9ca01266852c756b02e30944efec27350e3
Yaroslav <faloout@gmail.com>
2022-10-14 23:27:52 +0300

    39 : React JS - action creator, action type

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
   _state: {
     profilePage: {
@@ -40,7 +43,7 @@ let store = {
   },
 
   dispatch(action) {        // { type:'ADD-POST'}
-    if (action.type === "ADD-POST") {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 5,
         message: this._state.profilePage.newPostText,
@@ -50,11 +53,25 @@ let store = {
       this._state.profilePage.posts.push(newPost);
       this._state.profilePage.newPostText = "";
       this._callSubscriber(this._state);
-
-    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+      
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profilePage.newPostText = action.newText;
       this._callSubscriber(this._state);
     }
-  }
+  },
 }
-export default store;
+
+  export const addPostActionCreator = () => {
+    return {
+      type: ADD_POST
+    };
+  };
+
+  export const updateNewPostTextActionCreator = (text) => {
+    return {
+      type: UPDATE_NEW_POST_TEXT,
+      newText: text
+    };
+  };
+
+  export default store;
