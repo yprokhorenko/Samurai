bdc91762c67bdfa3620dafb9dbd288a5f0d2cc64
Yaroslav <faloout@gmail.com>
2023-01-20 21:36:34 +0200

    75 : redux-form введення (login) (не актуально - react hook form or formik)

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,13 +5,15 @@ import usersReducer from "./users-reducer";
 import { applyMiddleware, combineReducers, legacy_createStore as createStore } from "redux";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import  {reducer as formReducer} from 'redux-form';
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   navFriends: friendsReducer,
   usersPage: usersReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
