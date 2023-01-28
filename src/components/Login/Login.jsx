d25ca5b6d161c312f7375857dfedb016950e3a27
Yaroslav <faloout@gmail.com>
2023-01-28 23:51:29 +0200

    75 - (форма Login, 1/..): Formik + Yup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,83 @@
 import React from "react";
+import { Formik, Form, Field, handleSubmit,useFormik} from 'formik';
+import * as Yup from "yup";
+import s from './Login.module.css';
 
-const Login = (props) => {
-    return <h1>LOGIN</h1>;
+const Login = () => {
+  return (
+    <div  className={s.loginForm}>
+      <h1>LOGIN</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+
+const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      login: "", 
+      password: "",
+    },
+
+    onSubmit: (values) => {
+      console.log(values);
+    },
+
+    validationSchema: Yup.object({
+        login: Yup.string()
+        .min(5, "Must be 5 characters or more")
+        .max(10, "Must be 10 characters or less")
+        .required ("Required"),
+
+        password: Yup.string()
+        .min(8, "Must be 8 characters or more")
+        .max(15, "Must be 15 characters or less")
+        .required ("Required")
+    })
+
+  })
+  return (
+    <div>
+      <Formik>
+        <form onSubmit={formik.handleSubmit}>
+          <div className={s.inputContainer}>
+                <input placeholder="Login"
+                       name="login" 
+                       component="input"
+                       onChange={formik.handleChange}
+                       value={formik.values.login}
+                       onBlur={formik.handleBlur}
+                       />
+                       {formik.touched.login && formik.errors.login ? <span className={s.stringError}> {formik.errors.login}</span>:null}
+          </div>
+          <div className={s.inputContainer}>
+                <input
+                      placeholder="Password"
+                      name="password"
+                      component="input"
+                      onChange={formik.handleChange} 
+                      value={formik.values.password}
+                      onBlur={formik.handleBlur}
+                      />
+                      {formik.touched.password && formik.errors.password ? <span className={s.stringError}> {formik.errors.password}</span>:null}
+          </div>
+          <div className={s.checkbox}>
+                <input type="checkbox"
+                      name="rememberMe" 
+                      component="input"
+                      onChange={formik.handleChange} 
+                      value={formik.values.rememberMe}/>
+                      remember me
+          </div>
+                <div>
+                  <button type="submit">Login</button>
+                </div>
+        </form>
+      </Formik>
+    </div>
+  );
 }
+
 
 export default Login;
