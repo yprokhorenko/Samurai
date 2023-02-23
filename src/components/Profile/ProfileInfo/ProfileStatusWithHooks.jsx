f3e15a9b820e4ef068b05b7fcf1e61828aad1b6e
Yaroslav <faloout@gmail.com>
2023-02-23 13:37:05 +0200

    85 : hook, useEffect, хуки [мін коду]

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import s from "./ProfileInfo.module.css";
 
-const ProfileStatusWithHooks = (props) => {
-  
-  let [editMode, setEditMode] = useState(false);
-  let [status, setStatus] = useState(props.status)
+      const ProfileStatusWithHooks = (props) => {
+        
+        let [editMode, setEditMode] = useState(false);
+        let [status, setStatus] = useState(props.status)
 
-const activateEditMode = () => {
-  setEditMode(true);
-}
+      useEffect( () => {
+        setStatus(props.status);
+      }, [props.status])
 
-const deactivateEditMode = () => {
-    setEditMode(false);
-    props.updateStatus(status);
-}
+      const activateEditMode = () => {
+        setEditMode(true);
+      }
 
-const onStatusChange = (e) => {
-     setStatus(e.currentTarget.value)
-}
+      const deactivateEditMode = () => {
+          setEditMode(false);
+          props.updateStatus(status);
+      }
+
+      const onStatusChange = (e) => {
+          setStatus(e.currentTarget.value)
+      }
 
 
   return (
