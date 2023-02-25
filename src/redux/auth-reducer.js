fb11e55c5219b5aadda6f2bdf52696b5611259c2
Yaroslav <faloout@gmail.com>
2023-02-25 23:40:24 +0200

    90 : redux-ducks рефакторинг (рев'ю коду і виправлення що кидається в очі)

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,6 +1,6 @@
 import { authAPI } from "../api/api";
 
-const SET_USER_DATA = "SET_USER_DATA";
+const SET_USER_DATA = "auth/SET_USER_DATA";
 
 let initialState = {
   userId: null,
@@ -26,33 +26,29 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
   payload: { userId, email, login, isAuth},
 });
 
-export const getAuthUserData = () => (dispatch) => {
-  return authAPI.me().then(response => {
-    if (response.data.resultCode === 0) {
-      let { id, email, login } = response.data.data;
-      dispatch(setAuthUserData(id, email, login, true));
-    }
-  });
+export const getAuthUserData = () => async (dispatch) => {
+  let response = await authAPI.me();
+  if (response.data.resultCode === 0) {
+    let { id, email, login } = response.data.data;
+    dispatch(setAuthUserData(id, email, login, true));
+  }
 }; 
 
-export const login = (email, password, rememberMe, setStatus) => {
-  return(dispatch) => {
-  authAPI.login(email, password, rememberMe).then((response) => {
+export const login =
+  (email, password, rememberMe, setStatus) => async (dispatch) => {
+    let response = await authAPI.login(email, password, rememberMe);
     if (response.data.resultCode === 0) {
       dispatch(getAuthUserData());
+    } else {
+      setStatus(response.data.messages);
     }
-     else {
-      setStatus(response.data.messages)
-     }
-  });
-};}
+  };
 
-export const logout = () => (dispatch) => {
-  authAPI.logout().then(response => {
-    if (response.data.resultCode === 0) {
-      dispatch(setAuthUserData(null, null, null, false));
-    }
-  });
+export const logout = () => async (dispatch) => {
+  let response = await authAPI.logout();
+  if (response.data.resultCode === 0) {
+    dispatch(setAuthUserData(null, null, null, false));
+  }
 };
 
 
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -79,38 +79,35 @@ export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFe
 export const toggleFollowingProgress = (isFetching, userId) => ({type: TOGGLE_IS_FOLLOWING_PROGRESS, isFetching, userId})
 
 export const requestUsers = (requestedPage, pageSize) => {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(toggleIsFetching(true));
     // dispatch(setCurrentPage(requestedPage)) #81
-    usersAPI.requestUsers(requestedPage, pageSize).then((data) => {
-      dispatch(toggleIsFetching(false));
-      dispatch(setUsers(data.items));
-      dispatch(setTotalUsersCount(data.totalCount));
-    });
+    let data = await usersAPI.requestUsers(requestedPage, pageSize);
+    dispatch(toggleIsFetching(false));
+    dispatch(setUsers(data.items));
+    dispatch(setTotalUsersCount(data.totalCount));
   };
 };
 
 export const follow = (userId) => {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(toggleFollowingProgress(true, userId));
-    usersAPI.follow(userId).then(response => {
-      if (response.data.resultCode == 0) {
-        dispatch(followSuccess(userId));
-      }
-      dispatch(toggleFollowingProgress(false, userId));
-    });
+    let response = await usersAPI.follow(userId);
+    if (response.data.resultCode == 0) {
+      dispatch(followSuccess(userId));
+    }
+    dispatch(toggleFollowingProgress(false, userId));
   };
 };
 
 export const unfollow = (userId) => {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(toggleFollowingProgress(true, userId));
-    usersAPI.unfollow(userId).then(response => {
-      if (response.data.resultCode == 0) {
-        dispatch(unfollowSuccess(userId));
-      }
-      dispatch(toggleFollowingProgress(false, userId));
-    });
+    let response = await usersAPI.unfollow(userId);
+    if (response.data.resultCode == 0) {
+      dispatch(unfollowSuccess(userId));
+    }
+    dispatch(toggleFollowingProgress(false, userId));
   };
 };
   
